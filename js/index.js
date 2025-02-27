33d287e3823b80e72f06dc59119ab8454adfb765
youjeong Jeong <wjddbwjd1205@naver.com>
2025-02-27 12:58:52 +0900

    feat: 댓글 입력 및 동적 리스트 업데이트 기능 구현
    - 사용자가 댓글 입력 후 '등록' 버튼 클릭 시 댓글이 추가됨
    - 방문자 프로필 이미지와 함께 댓글 표시
    - 댓글 입력 후 자동으로 입력 필드 초기화

--- js/index.js
+++ js/index.js
@@ -1,19 +1,47 @@
 console.log(
   "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
+  " __      __  ______   __  __   ______     " +
+  "\n" +
+  "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
+  "\n" +
+  "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
+  "\n" +
+  " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
+  "\n" +
+  "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
+  "\n" +
+  "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
+  "\n" +
+  "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
   "color: #d81b60; font-size: 16px; font-weight: bold;"
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+document.addEventListener("DOMContentLoaded", function () {
+  const commentInput = document.getElementById("comment-input");
+  const commentList = document.getElementById("comment-list");
+  const submitButton = document.getElementById("submit-comment");
+
+  submitButton.addEventListener("click", function () {
+    const commentText = commentInput.value.trim();
+
+    if (commentText) {
+      // 새로운 댓글 요소 생성
+      const newComment = document.createElement("li");
+      newComment.classList.add("comment-item");
+      newComment.innerHTML = `
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지">
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">${commentText}</div>
+      `;
+
+      // 댓글 리스트에 추가
+      commentList.appendChild(newComment);
+
+      // 입력창 초기화
+      commentInput.value = "";
+    }
+  });
+});
+
